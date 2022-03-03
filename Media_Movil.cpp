f71f33377911d02cefaf53d73ef3e731d8432e60
Jose Angel Moneo <29139519+jamoneo@users.noreply.github.com>
2022-03-03 19:06:39 +0100

    Update Media_Movil.cpp

--- Media_Movil.cpp
+++ Media_Movil.cpp
@@ -22,8 +22,11 @@ void Media_Movil::init(int muestras){
   // Pfin=Muestras; //iniciamos los punteros de la cola
 }
 
-
+#ifdef MEDIA_LONG 
+void  Media_Movil::Media(long valor)
+#else
 void  Media_Movil::Media(float valor)
+#endif
 {  int Pfin;
    //SE saca el primer dato más antiguo de la cola
   S_Medidas-=Medidas[Pini]; //Resta el valor más inicio (antiguo) de la cola
@@ -38,6 +41,61 @@ void  Media_Movil::Media(float valor)
   if(Pini==Muestras) Pini=0; //Se reinicia si llegamos al final de la tabla
 
   return;
+}
+
+//toma la media movil actual
+#ifdef MEDIA_LONG 
+long Media_Movil::get()
+#else
+float Media_Movil::get()
+#endif
+{  return S_Medidas/(Muestras-1);}
+
+#ifdef MEDIA_LONG
+long Media_Movil::get_Mediana()
+#else
+float Media_Movil::get_Mediana()
+#endif
+{int i, j, l, m,kth;
+
+  #ifdef MEDIA_LONG
+   long x;
+  #else
+   float x;
+  #endif
+  kth=Muestras/2;
+
+  l = 0;
+  m = Muestras - 1;
+  while (l < m)
+  {
+    x = Medidas[kth];
+    i = l;
+    j = m;
+    do
+    {
+      while (Medidas[i] < x) i++;
+      while (x < Medidas[j]) j--;
+      if (i <= j)
+      {
+        #ifdef MEDIA_LONG
+          long t = Medidas[j];
+        #else
+          float t = Medidas[j];
+        #endif
+        Medidas[j] = Medidas[i];
+        Medidas[i] = t;
+        i++;
+        j--;
+      }
+    }
+    while (i <= j);
+    if (j < kth) l = i;
+    if (kth < i) m = j;
+  }
+  return Medidas[kth];
+
+  
 }
 
 //toma la media movil actual
